871e1f24ef13ed5248b97ad42e31ae4ade778587
Maurice Argoetti <maurice.argoetti@gmail.com>
2018-08-22 20:23:46 +0000

    automatically backed up by learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -4,12 +4,12 @@ class CreateHauntedHouses < ActiveRecord::Migration[4.2]
     create_table :haunted_houses do |t|
       t.string :name
       t.string :location
-      t.integer :theme
+      t.string :theme
       t.integer :price
       t.boolean :family_friendly
       t.datetime :opening_date
       t.datetime :closing_date
-      t.datetime :closing_date
+      t.string :get_description
 
   end
 end
